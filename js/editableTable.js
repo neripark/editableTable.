237910c9a5d4927516a216a1f751b1af497e58de
neriparktys <neripark@gmail.com>
2017-12-13 18:13:36 +0900

    v-forと固定データ動作

--- js/editableTable.js
+++ js/editableTable.js
@@ -1,6 +1,23 @@
+//テスト用
 var app1 = new Vue({
 	el: '#app1',
 	data: () => {
 		return { message: "hello vue!" };
 	}
 });
+
+
+//テーブル行
+var tableRow = new Vue({
+	el: '#vueTable',
+	data: () => {
+		return {rows: [
+			{guestName: 'おきあゆりょうたろう', hostName: 'neri', status: '送った'},
+			{guestName: 'いしだあきら', hostName: 'neri', status: '送った'},
+			{guestName: 'ほしそういちろう', hostName: 'neri', status: '○'},
+			{guestName: 'わかもとのりお', hostName: 'neri', status: '×'}
+		]}
+	}
+});
+
+console.log($('h1').text());
