3a581880f1c0d758c3731bf82f187b8b61ddfd46
neripark <neripark@gmail.com>
2018-01-02 01:08:35 +0900

    Vuex導入開始、チュートリアル動作OK

--- src/index.entry.js
+++ src/index.entry.js
@@ -1,9 +1,45 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import eTableComponent from './component/eTable.vue';
 import toast from './component/toast.vue';
 
 Vue.component('etable', eTableComponent);
 Vue.component('toast', toast);
+Vue.use(Vuex);
+
+
+//vuexテスト用
+const store = new Vuex.Store({
+  state: {
+    count:0
+  },
+  mutations: {
+    increment: state => state.count++,
+    decrement: state => state.count--
+  }
+});
+
+new Vue({
+  el: '#testVuex',
+  // data: {
+  //     cccc: store.state.count
+  // },
+  // dataではなぜいけないのか？最初からリアクティブではないから？
+  computed: {
+    cccc(){
+      return store.state.count
+    }
+  },
+  methods: {
+    up(){
+      store.commit('increment');
+    },
+    down(){
+      store.commit('decrement');
+    }
+  }
+});
+
 
 //インスタンス化
 new Vue({
