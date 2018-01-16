0e414f1f5259df219ac3bde87eb044a944c036f9
neriparktys <neripark@gmail.com>
2018-01-16 16:37:40 +0900

    不要コメントを削除

--- src/index.entry.js
+++ src/index.entry.js
@@ -39,40 +39,6 @@ Axios.get('/api/selectAllStatus.php')
   store.state.allStatus = response.data;
 });
 
-
-//vuexテスト用
-// const store = new Vuex.Store({
-//   state: {
-//     count:0
-//   },
-//   mutations: {
-//     increment: state => state.count++,
-//     decrement: state => state.count--
-//   }
-// });
-//
-// new Vue({
-//   el: '#testVuex',
-//   // data: {
-//   //     cccc: store.state.count
-//   // },
-//   // dataではなぜいけないのか？最初からリアクティブではないから？
-//   computed: {
-//     cccc(){
-//       return store.state.count
-//     }
-//   },
-//   methods: {
-//     up(){
-//       store.commit('increment');
-//     },
-//     down(){
-//       store.commit('decrement');
-//     }
-//   }
-// });
-
-
 //インスタンス化
 new Vue({
   el: '#eTable',
@@ -90,4 +56,3 @@ new Vue({
     toast
   }
 })
-//
