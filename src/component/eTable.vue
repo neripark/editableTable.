3e6efa70b5369f31a38a3b68265d8f92682d9930
neriparktys <neripark@gmail.com>
2018-01-16 18:08:29 +0900

    popOverのv-show、値の渡し分けOK

--- src/component/eTable.vue
+++ src/component/eTable.vue
@@ -1,35 +1,45 @@
 <template lang="html">
   <div id="eTable">
 
-    <button type="button" class="btn btn-secondary" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+    <!-- <button type="button" class="btn btn-secondary" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
       Popover on top
-    </button>
+    </button> -->
 
     <table class="table"><!-- bootstrap class -->
       <thead>
         <tr>
           <th scope="col">#</th>
           <th scope="col">Guest</th>
-          <th scope="col">Host</th>
-          <th scope="col">Status</th>
+          <th scope="col" @click="showChbGp(dataA)">{{dataA}}</th>
+          <th scope="col" @click="showChbGp(dataB)">{{dataB}}</th>
         </tr>
       </thead>
       <!-- <eTableRow :rows="this.rowsAll" :filteredStatus="this.selectedStatus"></eTableRow> -->
       <eTableRow :rows="rowsAll"></eTableRow>
     </table>
+    <popOver :uketori="displayCss" :vl="thr"></popOver>
   </div>
 </template>
 
 <script>
-import Axios from 'axios'
+//import Axios from 'axios'
 import eTableRow from './eTableRow.vue'
+import popOver from './popOver.vue'
 export default{
-  created: function(){
-    console.log('hogegege');
-    $(function(){
-      console.log('yomaremasia');
-      $('[data-toggle="popover"]').popover()
-    });
+  // created: function(){
+  //   //console.log('hogegege');
+  //   $(function(){
+  //     //console.log('yomaremasia');
+  //     $('[data-toggle="popover"]').popover()
+  //   });
+  // },
+  data: function() {
+    return {
+      dataA: "Host",
+      dataB: "Guest",
+      displayCss: false,
+      thr: "string"
+    };
   },
   computed: {
     rowsAll(){
@@ -37,11 +47,21 @@ export default{
     }
   },
   components: {
-    eTableRow
+    eTableRow, popOver
+  },
+  methods: {
+    showChbGp(val){
+      console.log(`checkbox ${val}`);
+      this.displayCss = !(this.displayCss);
+      this.thr = val;
+    }
   }
 }
 </script>
 
 <style lang="css">
   /* table{color: #57bd69;} */
+  #eTable{
+    position: relative;
+  }
 </style>
--- src/component/popover.vue
+++ src/component/popover.vue
@@ -1,13 +1,34 @@
 <template lang="html">
-
+  <!-- <div class="popover" :class="displayCss"> -->
+  <div v-show="uketori" class="popoverOrg">
+    <p class="lead">This is lead text. {{vl}}</p>
+    <div class="chbArea">
+      <p>{{options}}</p>
+      <label><input type="checkbox">テスト</label>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
-
+  props: ['uketori', 'vl']
 }
 </script>
 
-<style lang="css">
+<style lang="scss">
+.popoverOrg{
+  //display: none;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background-color: #ccc;
+  border-radius: 3px;
+  box-sizing: border-box;
+  width: 70%;
+  z-index: 1;
+  &.show{
+    display: block;
+  }
+}
 </style>
